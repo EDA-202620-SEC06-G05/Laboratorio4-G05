c920ee298f587314df7043d1a9da10941e203dbb
sergio Montejo Luna <s.montejol@uniandes.edu.co>
2026-09-06 14:01:21 -0500

    prints

--- App/view.py
+++ App/view.py
@@ -65,7 +65,8 @@ def load_data(control):
 
 def print_books_to_read(results):
     # TODO Imprimir los libros por leer
-    pass
+    for book in results["elements"]["elements"]:
+        prrint(f"Libro: {book['book_id']}, Usuario: {book['user_id']}")
 
 
 def print_tests_results(queue_results, stack_results):
@@ -82,7 +83,11 @@ def print_tests_results(queue_results, stack_results):
           f"{queue_results['dequeue_time']:.3f}", "[ms]")
 
     print("\nTiempos de ejecución para Pila: \n")
-
+    
+    print("Tiempo de ejecución para push:", f"{stack_results['push_time']:.3f}", "[ms]")
+    print("Tiempo de ejecución para top:", f"{stack_results['top_time']:.3f}", "[ms]")
+    print("Tiempo de ejecución para pop:", f"{stack_results['pop_time']:.3f}", "[ms]")
+    
     # TODO Imprimir los resultados de las pruebas de rendimiento de la pila
 
 
